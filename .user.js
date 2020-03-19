3395f5dedd16e30b9a4d4629720a760044f0b918
Alex Sander S.R. Silva <alex_sander_r.silva@hotmail.com>
2020-03-19 17:12:56 -0300

    Update .user.js

--- .user.js
+++ .user.js
@@ -1,22 +1,23 @@
 // ==UserScript==
-// @name         AutoTrimps-Zek
+// @name         AutoTrimps-Ray
 // @version      1.0-Zek
-// @namespace    https://Zorn192.github.io/AutoTrimps
-// @updateURL    https://Zorn192.github.io/AutoTrimps/.user.js
+// @namespace    https://github.com/Psycho-Ray/AutoTrimps/blob/gh-pages
+// @updateURL    https://github.com/Psycho-Ray/AutoTrimps/blob/gh-pages/.user.js
 // @description  Automate all the trimps!
 // @author       zininzinin, spindrjr, Ishkaru, genBTC, Zeker0
 // @include      *trimps.github.io*
 // @include      *kongregate.com/games/GreenSatellite/trimps
-// @connect      *Zorn192.github.io/AutoTrimps*
+// @connect      *github.com/Psycho-Ray/AutoTrimps/blob/gh-pages*
 // @connect      *trimps.github.io*
 // @connect      self
 // @grant        GM_xmlhttpRequest 
 // ==/UserScript==
 
 var script = document.createElement('script');
-script.id = 'AutoTrimps-Zek';
+script.id = 'AutoTrimps-Ray';
 //This can be edited to point to your own Github Repository URL.
-script.src = 'https://Zorn192.github.io/AutoTrimps/AutoTrimps2.js';
+script.src = 'https://github.com/Psycho-Ray/AutoTrimps/blob/gh-pages/AutoTrimps2.js';
 //script.setAttribute('crossorigin',"use-credentials");
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
+
