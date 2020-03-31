d98091bfed339e088d5d74ad63cab1e52a68e27f
Alex Sander S.R. Silva <alex_sander_r.silva@hotmail.com>
2020-03-31 15:51:11 -0300

    Update calc.js

--- modules/calc.js
+++ modules/calc.js
@@ -172,42 +172,69 @@ function calcOurDmg(minMaxAvg, incStance, incFlucts) {
   var fluctuation = .2;
 	var maxFluct = -1;
 	var minFluct = -1;
-	if (game.jobs.Amalgamator.owned > 0){
+	
+	//Amalgamator
+	if (game.jobs.Amalgamator.owned > 0) {
 		number *= game.jobs.Amalgamator.getDamageMult();
 	}
-	if (game.challenges.Electricity.stacks > 0) {
-		number *= (1 - (game.challenges.Electricity.stacks * 0.1));
-	}
+	
+	//Anticipation
 	if (getPageSetting('45stacks') == false && game.global.antiStacks > 0) {
 		number *= ((game.global.antiStacks * game.portal.Anticipation.level * game.portal.Anticipation.modifier) + 1);
 	}
 	if (getPageSetting('45stacks') == true && game.global.antiStacks > 0) {
 		number *= ((45 * game.portal.Anticipation.level * game.portal.Anticipation.modifier) + 1);
 	}
-	if (game.global.mapBonus > 0){
+	
+	//Map Bonus
+	if (game.global.mapBonus > 0) {
 	    var mapBonus = game.global.mapBonus;
             if (game.talents.mapBattery.purchased && mapBonus == 10) mapBonus *= 2;
 		number *= ((mapBonus * .2) + 1);
 	}
-	if (game.global.achievementBonus > 0){
+	
+	//Achievements
+	if (game.global.achievementBonus > 0) {
 		number *= (1 + (game.global.achievementBonus / 100));
 	}
-	if (game.global.challengeActive == "Discipline"){
+	
+	//Discipline
+	if (game.global.challengeActive == "Discipline") {
 		fluctuation = .995;
 	}
-	else if (game.portal.Range.level > 0){
+	
+	//Range
+	else if (game.portal.Range.level > 0) {
 		minFluct = fluctuation - (.02 * game.portal.Range.level);
 	}
-	if (game.global.challengeActive == "Decay"){
+	
+	//Decay
+	if (game.global.challengeActive == "Decay") {
 		number *= 5;
 		number *= Math.pow(0.995, game.challenges.Decay.stacks);
 	}
-	if (game.global.roboTrimpLevel > 0){
+	
+	//Electricity
+	if (game.challenges.Electricity.stacks > 0) {
+		number *= 1 - (game.challenges.Electricity.stacks * 0.1);
+	}
+	
+	//Life
+	if (game.global.challengeActive == "Life") {
+		number *= game.challenges.Life.getHealthMult();
+	}
+	
+	//Robo Trimp
+	if (game.global.roboTrimpLevel > 0) {
 		number *= ((0.2 * game.global.roboTrimpLevel) + 1);
 	}
+	
+	//Lead
 	if (game.global.challengeActive == "Lead" && ((game.global.world % 2) == 1)){
 		number *= 1.5;
 	}
+	
+	//Battle Goldens
 	if (game.goldenUpgrades.Battle.currentBonus > 0) {
 		number *= game.goldenUpgrades.Battle.currentBonus + 1;
 	}
@@ -253,9 +280,6 @@ function calcOurDmg(minMaxAvg, incStance, incFlucts) {
 	if (Fluffy.isRewardActive('voidSiphon') && game.stats.totalVoidMaps.value) {
 			number *= (1 + (game.stats.totalVoidMaps.value * 0.05));
 	}
-	if (game.global.challengeActive == "Life") {
-		number *= game.challenges.Life.getHealthMult();
-	}
 	if (game.singleRunBonuses.sharpTrimps.owned){
 		number *= 1.5;
 	}
@@ -268,9 +292,9 @@ function calcOurDmg(minMaxAvg, incStance, incFlucts) {
 	if (game.global.challengeActive == "Daily" && game.talents.daily.purchased){
 		number *= 1.5;
 	}
-    	if (game.global.challengeActive == 'Lead' && game.global.world % 2 == 1 && game.global.world != 179) {
-        	number /= 1.5;
-    	}
+    if (game.global.challengeActive == 'Lead' && game.global.world % 2 == 1 && game.global.world != 179) {
+        number /= 1.5;
+    }
 	if (game.global.challengeActive == "Daily"){
 		if (typeof game.global.dailyChallenge.minDamage !== 'undefined'){
 			if (minFluct == -1) minFluct = fluctuation;
