9e08bd95218f8afae13cbb39f290d3c7729684f4
Alex Sander S.R. Silva <alex_sander_r.silva@hotmail.com>
2020-04-27 02:10:01 -0300

    Update calc.js

--- modules/calc.js
+++ modules/calc.js
@@ -435,7 +435,7 @@ function calcSpire(what, cell, name) {
 
     //Enemy on the Target Cell
     var enemy = (name) ? name : game.global.gridArray[exitCell].name;
-    var base = (what == "attack") ? game.global.getEnemyAttack(100, enemy, true) : (calcEnemyBaseHealth(game.global.world, 100, enemy) * 2);
+    var base = (what == "attack") ? game.global.getEnemyAttack(100, null, true) : (calcEnemyBaseHealth(game.global.world, 100, null, true) * 2);
     var mod = (what == "attack") ? 1.17 : 1.14;
 
     //Spire Num
