24d9f2c5b8c8aee5b4285f4a03df88eef61eb621
Alex Sander S.R. Silva <alex_sander_r.silva@hotmail.com>
2020-04-28 21:44:50 -0300

    Update calc.js

--- modules/calc.js
+++ modules/calc.js
@@ -569,7 +569,7 @@ function calcSpecificEnemyHealth(world, map, cell) {
     if (!cell) cell = (!map) ? getCurrentWorldCell().level : (getCurrentMapCell() ? getCurrentMapCell().level : 1);
 
     //Init
-    var enemy = (!map) ? game.global.gridArray[cell-1] : (getCurrentMapCell() ? : game.global.mapGridArray[cell-1] : "Chimp");
+    var enemy = (!map) ? game.global.gridArray[cell-1] : (getCurrentMapCell() ? game.global.mapGridArray[cell-1] : "Chimp");
     var corrupt = enemy.hasOwnProperty("corrupted");
     var healthy = enemy.hasOwnProperty("healthy");
     var name = (corrupt || healthy) ? "Chimp" : enemy.name;
