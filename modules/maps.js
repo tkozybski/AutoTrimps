d9755318366f9fd1117a34f055ebe97263c8c26e
Alex Sander S.R. Silva <alex_sander_r.silva@hotmail.com>
2020-04-30 03:01:51 -0300

    Update maps.js

--- modules/maps.js
+++ modules/maps.js
@@ -1,7 +1,7 @@
 //Helium
 
 MODULES.maps={};
-MODULES.maps.numHitsSurvived=20;
+MODULES.maps.numHitsSurvived=15;
 MODULES.maps.farmOnLowHealth=true;
 MODULES.maps.LeadfarmingCutoff=10;
 MODULES.maps.NomfarmingCutoff=10;
