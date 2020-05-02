6c2e6d63f98f0da4b11405075658d3cd2db3675f
Alex Sander S.R. Silva <alex_sander_r.silva@hotmail.com>
2020-05-01 22:52:37 -0300

    Update equipment.js

--- modules/equipment.js
+++ modules/equipment.js
@@ -1,6 +1,6 @@
 MODULES["equipment"] = {};
 MODULES["equipment"].numHitsSurvived = 30;
-MODULES["equipment"].numHitsSurvivedScry = 60;
+MODULES["equipment"].numHitsSurvivedScry = 80;
 MODULES["equipment"].capDivisor = 10;
 MODULES["equipment"].alwaysLvl2 = getPageSetting('always2');
 MODULES["equipment"].waitTill60 = true;
