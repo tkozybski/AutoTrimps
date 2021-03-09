f71022f862784f9c87a74010929aa52c0906f1ae
Alex Sander S.R. Silva <alex_sander_r.silva@hotmail.com>
2021-03-09 20:48:45 -0300

    Update .user.js

--- .user.js
+++ .user.js
@@ -2,15 +2,12 @@
 // @name         AutoTrimps-Ray
 // @version      0.1-Ray
 // @namespace    https://Psycho-Ray.github.io/AutoTrimps
-// @updateURL    https://Psycho-Ray.github.io/AutoTrimps/.user.js
+// @downloadURL  https://Psycho-Ray.github.io/AutoTrimps/.user.js
 // @description  Automate all the trimps!
 // @author       zininzinin, spindrjr, Ishkaru, genBTC, Zeker0, Psycho-Ray
 // @include      *trimps.github.io*
 // @include      *kongregate.com/games/GreenSatellite/trimps
-// @connect      *github.com/Psycho-Ray/AutoTrimps/blob/gh-pages*
-// @connect      *trimps.github.io*
-// @connect      self
-// @grant        GM_xmlhttpRequest 
+// @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
 var script = document.createElement('script');
@@ -20,4 +17,3 @@ script.src = 'https://Psycho-Ray.github.io/AutoTrimps/AutoTrimps2.js';
 //script.setAttribute('crossorigin',"use-credentials");
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
-
