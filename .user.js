85ca615462dc18c79d7d90369a6c055645fc5731
Alex Sander S.R. Silva <alex_sander_r.silva@hotmail.com>
2021-03-09 20:53:48 -0300

    Update .user.js

--- .user.js
+++ .user.js
@@ -3,6 +3,7 @@
 // @version      0.1-Ray
 // @namespace    https://Psycho-Ray.github.io/AutoTrimps
 // @downloadURL  https://Psycho-Ray.github.io/AutoTrimps/.user.js
+// @updateURL    https://Psycho-Ray.github.io/AutoTrimps/.user.js
 // @description  Automate all the trimps!
 // @author       zininzinin, spindrjr, Ishkaru, genBTC, Zeker0, Psycho-Ray
 // @include      *trimps.github.io*
