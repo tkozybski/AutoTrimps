c5d9a103e82381939146507aeef488c8a1bc923c
Alex Sander S.R. Silva <alex_sander_r.silva@hotmail.com>
2021-03-18 01:49:03 -0300

    Update maps.js

--- modules/maps.js
+++ modules/maps.js
@@ -21,7 +21,7 @@ MODULES.maps.farmOnLowHealth = true; //Will force farming for health
 MODULES.maps.spireHitsSurvived = 0.25; //1 is actually 8 hits+ using Heap. Set to something low to save nurseries past magma
 MODULES.maps.scryerHitsMult = 6; //This is a multiplier to your "numHitsSurvived", and only works if Scry on Corrupted is ON
 MODULES.maps.voidHitsMult = 0.25; //This is a multiplier to your "numHitsSurvived", and only works at your void map zones
-MODULES.maps.voidHDMult = MODULES.maps.voidHitsMult; //This is a multiplier to your "numHitsSurvived", and only works at your void map zones
+MODULES.maps.voidHDMult = 1 / MODULES.maps.voidHitsMult; //This is a multiplier to your "numHitsSurvived", and only works at your void map zones
 
 var isFarming = false;
 var doVoids = false;
