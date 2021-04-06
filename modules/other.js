3127c3a05b26629c72c335f54e9f98cc5ead080a
Psycho-Ray <alex_sander_r.silva@hotmail.com>
2021-04-06 13:27:59 -0300

    Allow Trimpicide while Farming Maps before Spire

--- modules/other.js
+++ modules/other.js
@@ -1783,7 +1783,7 @@ function dailyBWraiding() {
 function trimpcide() {
 if (game.portal.Anticipation.level > 0) {
 	var antistacklimit = (game.talents.patience.purchased) ? 45 : 30;
-	if (game.global.fighting && ((game.jobs.Amalgamator.owned > 0) ? Math.floor((new Date().getTime() - game.global.lastSoldierSentAt) / 1000) : Math.floor(game.global.lastBreedTime / 1000)) >= antistacklimit && (game.global.antiStacks < antistacklimit || antistacklimit == 0 && game.global.antiStacks >= 1) && !game.global.spireActive)
+	if (game.global.fighting && ((game.jobs.Amalgamator.owned > 0) ? Math.floor((new Date().getTime() - game.global.lastSoldierSentAt) / 1000) : Math.floor(game.global.lastBreedTime / 1000)) >= antistacklimit && (game.global.antiStacks < antistacklimit || antistacklimit == 0 && game.global.antiStacks >= 1) && (!game.global.spireActive || game.global.mapsActive))
               forceAbandonTrimps();
 	if (game.global.fighting && ((game.jobs.Amalgamator.owned > 0) ? Math.floor((new Date().getTime() - game.global.lastSoldierSentAt) / 1000) : Math.floor(game.global.lastBreedTime / 1000)) >= antistacklimit && game.global.antiStacks < antistacklimit && game.global.mapsActive) {
 	      if (getCurrentMapObject().location == "Void") {
