04856b5e4c9611ad9ec34430d18b6c256bbe67b1
Psycho-Ray <alex_sander_r.silva@hotmail.com>
2021-04-16 01:17:21 -0300

    Fixing Daily He/Hr Portal

--- modules/portal.js
+++ modules/portal.js
@@ -85,37 +85,36 @@ function dailyAutoPortal() {
             var bestHeHrZone = game.stats.bestHeliumHourThisRun.atZone;
             var myHeliumHr = game.stats.heliumHour.value();
             var heliumHrBuffer = Math.abs(getPageSetting('dHeliumHrBuffer'));
-            if (!aWholeNewWorld) {
+            if (!aWholeNewWorld)
                 heliumHrBuffer *= MODULES["portal"].bufferExceedFactor;
-                var bufferExceeded = myHeliumHr < bestHeHr * (1 - (heliumHrBuffer / 100));
-                if (bufferExceeded && game.global.world >= minZone) {
-                    OKtoPortal = true;
-                    if (aWholeNewWorld)
-                        zonePostpone = 0;
-                }
-                if (heliumHrBuffer == 0 && !aWholeNewWorld)
-                    OKtoPortal = false;
-                if (OKtoPortal && zonePostpone == 0) {
-                    zonePostpone += 1;
-                    debug("My HeliumHr was: " + myHeliumHr + " & the Best HeliumHr was: " + bestHeHr + " at zone: " + bestHeHrZone, "portal");
-                    cancelTooltip();
-                    tooltip('confirm', null, 'update', '<b>Auto Portaling NOW!</b><p>Hit Delay Portal to WAIT 1 more zone.', 'zonePostpone+=1', '<b>NOTICE: Auto-Portaling in 5 seconds....</b>', 'Delay Portal');
-                    setTimeout(cancelTooltip, MODULES["portal"].timeout);
-                    setTimeout(function() {
-                        if (zonePostpone >= 2)
-                            return;
-                        if (OKtoPortal) {
-                            abandonDaily();
-                            document.getElementById('finishDailyBtnContainer').style.display = 'none';
-                        }
-                        if (autoTrimpSettings.dHeliumHourChallenge.selected != 'None' && getPageSetting('u1daily') == false)
-                            doPortal(autoTrimpSettings.dHeliumHourChallenge.selected);
-			else if (autoTrimpSettings.RdHeliumHourChallenge.selected != 'None' && getPageSetting('u1daily') == true)
-                            doPortal(autoTrimpSettings.RdHeliumHourChallenge.selected);
-                        else
-                            doPortal();
-                    }, MODULES["portal"].timeout + 100);
-                }
+            var bufferExceeded = myHeliumHr < bestHeHr * (1 - (heliumHrBuffer / 100));
+            if (bufferExceeded && game.global.world >= minZone) {
+                OKtoPortal = true;
+                if (aWholeNewWorld)
+                    zonePostpone = 0;
+            }
+            if (heliumHrBuffer == 0 && !aWholeNewWorld)
+                OKtoPortal = false;
+            if (OKtoPortal && zonePostpone == 0) {
+                zonePostpone += 1;
+                debug("My HeliumHr was: " + myHeliumHr + " & the Best HeliumHr was: " + bestHeHr + " at zone: " + bestHeHrZone, "portal");
+                cancelTooltip();
+                tooltip('confirm', null, 'update', '<b>Auto Portaling NOW!</b><p>Hit Delay Portal to WAIT 1 more zone.', 'zonePostpone+=1', '<b>NOTICE: Auto-Portaling in 5 seconds....</b>', 'Delay Portal');
+                setTimeout(cancelTooltip, MODULES["portal"].timeout);
+                setTimeout(function() {
+                    if (zonePostpone >= 2)
+                        return;
+                    if (OKtoPortal) {
+                        abandonDaily();
+                        document.getElementById('finishDailyBtnContainer').style.display = 'none';
+                    }
+                    if (autoTrimpSettings.dHeliumHourChallenge.selected != 'None' && getPageSetting('u1daily') == false)
+                        doPortal(autoTrimpSettings.dHeliumHourChallenge.selected);
+                    else if (autoTrimpSettings.RdHeliumHourChallenge.selected != 'None' && getPageSetting('u1daily') == true)
+                        doPortal(autoTrimpSettings.RdHeliumHourChallenge.selected);
+                    else
+                        doPortal();
+                }, MODULES["portal"].timeout + 100);
             }
         }
     }
@@ -391,7 +390,7 @@ function RdailyAutoPortal() {
                         }
                         if (autoTrimpSettings.RdHeliumHourChallenge.selected != 'None' && getPageSetting('u2daily') == false)
                             RdoPortal(autoTrimpSettings.RdHeliumHourChallenge.selected);
-			else if (autoTrimpSettings.dHeliumHourChallenge.selected != 'None' && getPageSetting('u2daily') == true)
+			            else if (autoTrimpSettings.dHeliumHourChallenge.selected != 'None' && getPageSetting('u2daily') == true)
                             RdoPortal(autoTrimpSettings.dHeliumHourChallenge.selected);
                         else
                             RdoPortal();
