3bc2df4fdcbeb62e04bc1a5596494827d3f88e1f
Psycho-Ray <alex_sander_r.silva@hotmail.com>
2021-04-18 02:00:00 -0300

    Fixing Map Farming above current zone... again

--- modules/maps.js
+++ modules/maps.js
@@ -23,8 +23,8 @@ MODULES.maps.scryerHDMult = 4; //This is a divisor to your "mapCutOff" and "farm
 MODULES.maps.scryerHitsMult = 6; //This is a multiplier to your "numHitsSurvived", and only works if Scry on Corrupted is ON (Domination ignores this)
 MODULES.maps.voidHDMult = 1; //This is a multiplier to your "mapCutOff and farming H:D", and only works at your void map zones
 MODULES.maps.voidHitsMult = 2; //This is a multiplier to your "numHitsSurvived", and only works at your void map zones
-MODULES.maps.spireHD = 32; //4 is actually 1 hit in D stance
-MODULES.maps.spireHitsSurvived = 1.25; //1 is actually 8 hits+ using Heap. Set to something low to save nurseries past magma
+MODULES.maps.spireHD = 4; //4 is actually 1 hit in D stance
+MODULES.maps.spireHitsSurvived = 0.25; //1 is actually 8 hits+ using Heap. Set to something low to save nurseries past magma
 
 var isFarming = false;
 var doVoids = false;
@@ -890,7 +890,7 @@ function autoMap() {
 
                     //Update our control flags
                     extraMapLevels = getExtraMapLevels();
-                    gotBetterMod = $mapLevelInput.value + getExtraMapLevels() > altSiphLevel && testMapSpecialModController(true);
+                    gotBetterMod = parseInt($mapLevelInput.value) + getExtraMapLevels() > altSiphLevel && testMapSpecialModController(true);
                 }
                 else gotBetterMod = testMapSpecialModController(tryBetterMod);
             }
