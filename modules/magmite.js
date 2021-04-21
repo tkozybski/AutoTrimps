6db8eba2252da2b50391d500e284bf11b8be1556
Psycho-Ray <alex_sander_r.silva@hotmail.com>
2021-04-21 12:40:26 -0300

    Auto Fuel zone and Zones to Fuel

--- modules/magmite.js
+++ modules/magmite.js
@@ -181,7 +181,7 @@ function autoGenerator() {
     //Auto Fuel Zone
     if (MODULES.magmite.autoFuelZone) {
         setPageSetting("fuellater", Math.max(230, 230 + 2 * game.generatorUpgrades.Supply.upgrades - MODULES.magmite.zonesBeforeMaxSupply));
-        setPageSetting("fuelend", getPageSetting("fuellater") + MODULES.magmite.zonesToFuel - MODULES.magmite.zonesBeforeMaxSupply);
+        setPageSetting("fuelend", getPageSetting("fuellater") + MODULES.magmite.zonesToFuel);
     }
 
     //Dimensional Generator locked
