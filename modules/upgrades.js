e1f12479468e53201974fd7abe136517f5efb873
Psycho-Ray <alex.sander@botcity.com.br>
2022-01-19 05:39:01 -0300

    Fix gather early game

--- modules/upgrades.js
+++ modules/upgrades.js
@@ -151,9 +151,9 @@ function buyUpgrades() {
         var needScientists = game.global.challengeActive != 'Scientist' && !game.upgrades.Scientists.done;
         if (upgrade == 'Shieldblock' && !getPageSetting('BuyShieldblock')) continue;
         if (upgrade == 'Bloodlust' && game.global.challengeActive == 'Scientist' && getPageSetting('BetterAutoFight')) continue;
-        if (upgrade == 'Bloodlust' && needScientists && game.resources.science < 160 && game.resources.food < 250) continue;
-        if (upgrade == 'Bloodlust' && needMiner && needScientists && game.resources.science < 220) continue;
-        if (upgrade == 'Bloodlust' && needMiner && game.resources.science < 120) continue;
+        if (upgrade == 'Bloodlust' && needScientists && game.resources.science.owned < 160 && game.resources.food.owned < 250) continue;
+        if (upgrade == 'Bloodlust' && needMiner && needScientists && game.resources.science.owned < 220) continue;
+        if (upgrade == 'Bloodlust' && needMiner && game.resources.science.owned < 120) continue;
 
         if (game.upgrades.Scientists.done < game.upgrades.Scientists.allowed && upgrade != 'Scientists' && upgrade != 'Miners') continue;
         buyUpgrade(upgrade, true, true);
